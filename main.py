75754ff006cb3767213e44df48d63ecd6c4614c8
parsarah366p-bit <parsarah366p@gmail.com>
2026-08-05 17:10:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ os.environ["ALLOWED_HOSTS"] = "*"
 os.environ["UVICORN_FORWARDED_ALLOW_IPS"] = "*"
 os.environ["UVICORN_PROXY_HEADERS"] = "true"
 
-from telegram_mcp.cli import main
+from telegram_mcp.runner import main
 
 if __name__ == "__main__":
     main()
